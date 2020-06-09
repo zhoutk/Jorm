e0fca5b7a248eec4d2ab25b1708fd6245a2a32f1
zhoutk <zhoutk@189.cn>
2020-06-09 09:47:05 +0800

    init.

---
+++ Jorm/Jorm.cpp
@@ -0,0 +1,15 @@
+// Jorm.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World!" << endl;
+	system("pause");
+	return 0;
+}
+
