8d164220b58c6ace6d82188982d21e14ff9f96d1
zhoutk <zhoutk@189.cn>
2020-06-11 16:26:01 +0800

    pointer.

--- common/Rjson.h
+++ common/Rjson.h
@@ -10,16 +10,18 @@ using namespace std;
 using namespace rapidjson;
 
 class Rjson{
+private:
+	Document* json;
 
 public:
 	Rjson() {
 		json = new Document();
-		(*json).SetObject();
+		json->SetObject();
 	}
 
 	Rjson(char* jstr) {
 		json = new Document();
-		(*json).Parse(jstr);
+		json->Parse(jstr);
 	}
 
 	Rjson(string jstr) {
@@ -39,14 +41,14 @@ public:
 		string* newK = new string(k);
 		Value aInt(kNumberType);
 		aInt.SetInt(v);
-		(*json).AddMember(StringRef((*newK).c_str()), aInt, (*json).GetAllocator());
+		json->AddMember(StringRef(newK->c_str()), aInt, json->GetAllocator());
 	}
 
 	void AddValueString(string k, string v) {
 		string* newK = new string(k);
 		Value aStr(kStringType);
-		aStr.SetString(v.c_str(), (*json).GetAllocator());
-		(*json).AddMember(StringRef((*newK).c_str()), aStr, (*json).GetAllocator());
+		aStr.SetString(v.c_str(), json->GetAllocator());
+		json->AddMember(StringRef(newK->c_str()), aStr, json->GetAllocator());
 	}
 
 	void AddValueObjectArray(string k, vector<Rjson> arr) {
@@ -56,17 +58,17 @@ public:
 		for (int i = 0; i < len; i++) {
 			Value arow(kObjectType);
 			Document* al = arr.at(i).GetOriginRapidJson();
-			for (auto iter = (*al).MemberBegin(); iter != (*al).MemberEnd(); ++iter)
+			for (auto iter = al->MemberBegin(); iter != al->MemberEnd(); ++iter)
 			{
-				arow.AddMember(iter->name, iter->value, (*json).GetAllocator());
+				arow.AddMember(iter->name, iter->value, json->GetAllocator());
 			}
-			rows.PushBack(arow, (*json).GetAllocator());
+			rows.PushBack(arow, json->GetAllocator());
 		}
-		(*json).AddMember(StringRef((*newK).c_str()), rows, (*json).GetAllocator());
+		json->AddMember(StringRef(newK->c_str()), rows, json->GetAllocator());
 	}
 
 	void GetValueAndTypeByKey(string key, string* v, bool* v_number) {
-		Value& value = (*json).FindMember(key.c_str())->value;
+		Value& value = json->FindMember(key.c_str())->value;
 		if (value.IsInt() || value.IsInt64() || value.IsFloat() || value.IsDouble()) {
 			*v_number = true;
 			std::stringstream s;
@@ -85,7 +87,7 @@ public:
 
 	vector<string> GetAllKeys() {
 		vector<string> keys;
-		for (auto iter = (*json).MemberBegin(); iter != (*json).MemberEnd(); ++iter)
+		for (auto iter = json->MemberBegin(); iter != json->MemberEnd(); ++iter)
 		{
 			keys.push_back((iter->name).GetString());
 		}
@@ -93,13 +95,13 @@ public:
 	}
 
 	bool IsObject() {
-		return (*json).IsObject();
+		return json->IsObject();
 	}
 
 	string GetJsonString() {
 		StringBuffer strBuffer;
 		Writer<StringBuffer> writer(strBuffer);
-		(*json).Accept(writer);
+		json->Accept(writer);
 		return strBuffer.GetString();
 	}
 
@@ -108,6 +110,4 @@ public:
 			delete json;
 	}
 
-private:
-	Document* json;
 };
