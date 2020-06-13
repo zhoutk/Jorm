7efd959a13932bc703b75764f9963b4fee775be4
zhoutk <zhoutk@189.cn>
2020-06-13 20:46:06 +0800

    mark AddValueObjectArray, need create new obj for Value? I remember it's wrong, but now is right, so mark.

--- Sqlit3/Sqlit3Db.h
+++ Sqlit3/Sqlit3Db.h
@@ -121,7 +121,7 @@ public:
 
 private:
 	Rjson ExecQuerySql(string aQuery, vector<string> fields) {
-		Rjson rs;// = Utils::MakeJsonObjectForFuncReturn(STSUCCESS);
+		Rjson rs = Utils::MakeJsonObjectForFuncReturn(STSUCCESS);
 		sqlite3_stmt* stmt = NULL;
 		sqlite3* handle = getHandle();
 		string u8Query = Utils::UnicodeToU8(aQuery);
@@ -174,7 +174,7 @@ private:
 
 					}
 				}
-				arr.push_back(Rjson(al));
+				arr.push_back(al);
 			}
 			rs.AddValueObjectArray("data", arr);
 		}
