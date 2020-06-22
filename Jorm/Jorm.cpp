76ba8a14b6425b5d1c3704954412dc7c834cda61
zhoutk <zhoutk@189.cn>
2020-06-22 15:32:22 +0800

    test 100 insert, results is 17.639 s.

--- Jorm/Jorm.cpp
+++ Jorm/Jorm.cpp
@@ -13,8 +13,8 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	DbBase * db = new DbBase("D:\\codes\\Jorm\\Sqlit3\\db.db");
 
-	vector<Rjson> els;
 	char* strTmp = "abcdefghijklmnopqrstuvwxyz";
+	clock_t start = clock();
 	for (int i = 0; i < 100; i++) {
 		Rjson qObj;
 		qObj.AddValueInt("password", rand());
@@ -26,41 +26,59 @@ int _tmain(int argc, _TCHAR* argv[])
 			uname.append(1, strTmp[rand() % 26]);
 		}
 		qObj.AddValueString("username", uname);
-		els.push_back(qObj);
+		db->create("users", qObj);
 	}
-
-	clock_t start = clock();
-	Rjson rs = db->insertBatch("users", els);			//, Utils::MakeVectorInitFromString("id,username,password")
 	clock_t end = clock();
 	cout << "花费了" << (double)(end - start) / CLOCKS_PER_SEC << "秒" << endl;
 
+	//vector<Rjson> els;
+	//char* strTmp = "abcdefghijklmnopqrstuvwxyz";
+	//for (int i = 0; i < 100; i++) {
+	//	Rjson qObj;
+	//	qObj.AddValueInt("password", rand());
+	//	int len = rand() % 5 + 5;
+	//	string uname = "";
+	//	for (int j = 0; j < len; j++) {
+	//		int dd = rand() % 26;
+	//		char ddd = strTmp[rand() % 26];
+	//		uname.append(1, strTmp[rand() % 26]);
+	//	}
+	//	qObj.AddValueString("username", uname);
+	//	els.push_back(qObj);
+	//}
 
-	vector<string> keys = rs.GetAllKeys();
+	//clock_t start = clock();
+	//Rjson rs = db->insertBatch("users", els);			//, Utils::MakeVectorInitFromString("id,username,password")
+	//clock_t end = clock();
+	//cout << "花费了" << (double)(end - start) / CLOCKS_PER_SEC << "秒" << endl;
 
-	int len = keys.size();
-	for (int i = 0; i < len; i++) {
-		string v, key = keys[i];
-		int vType;
-		rs.GetValueAndTypeByKey(key, &v, &vType);
-		if (vType == 4) {
-			cout << "    " << key << " is array: " << endl;
-			cout << "-------------------------------------" << endl;
-			vector<Rjson> arr = rs.GetArrayByKey(key);
-			for (size_t j = 0; j < arr.size(); j++) {
-				Rjson al = arr[j];
-				vector<string> subKeys = al.GetAllKeys();
-				for (size_t k = 0; k < subKeys.size(); k++) {
-					string sv;
-					int nstype;
-					al.GetValueAndTypeByKey(subKeys[k], &sv, &nstype);
-					cout << "    " << subKeys[k] << ", value: " << sv << endl;
-				}
-				cout << endl;
-			}
-			//cout << "-------------------------------------" << endl;
-		}else
-			cout << "key: " << key << ", value: " << v << endl;
-	}
+
+	//vector<string> keys = rs.GetAllKeys();
+
+	//int len = keys.size();
+	//for (int i = 0; i < len; i++) {
+	//	string v, key = keys[i];
+	//	int vType;
+	//	rs.GetValueAndTypeByKey(key, &v, &vType);
+	//	if (vType == 4) {
+	//		cout << "    " << key << " is array: " << endl;
+	//		cout << "-------------------------------------" << endl;
+	//		vector<Rjson> arr = rs.GetArrayByKey(key);
+	//		for (size_t j = 0; j < arr.size(); j++) {
+	//			Rjson al = arr[j];
+	//			vector<string> subKeys = al.GetAllKeys();
+	//			for (size_t k = 0; k < subKeys.size(); k++) {
+	//				string sv;
+	//				int nstype;
+	//				al.GetValueAndTypeByKey(subKeys[k], &sv, &nstype);
+	//				cout << "    " << subKeys[k] << ", value: " << sv << endl;
+	//			}
+	//			cout << endl;
+	//		}
+	//		//cout << "-------------------------------------" << endl;
+	//	}else
+	//		cout << "key: " << key << ", value: " << v << endl;
+	//}
 
 	//cout << "22222222 the result is : " << rs.GetJsonString() << endl;
 
