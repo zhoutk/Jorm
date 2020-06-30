642b3fb3d1b52570685d65bf081380c211c30562
zhoutk <zhoutk@189.cn>
2020-06-30 09:14:36 +0800

    databse time alter to local zone time, tunning postgres docker date config, must restart docker.

--- Postgres/users.sql
+++ Postgres/users.sql
@@ -1,18 +1,18 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : pg
+ Source Server         : pq
  Source Server Type    : PostgreSQL
- Source Server Version : 110007
+ Source Server Version : 110008
  Source Host           : localhost:5432
  Source Catalog        : Jorm
  Source Schema         : public
 
  Target Server Type    : PostgreSQL
- Target Server Version : 110007
+ Target Server Version : 110008
  File Encoding         : 65001
 
- Date: 29/06/2020 21:32:37
+ Date: 30/06/2020 09:09:45
 */
 
 
@@ -24,7 +24,7 @@ CREATE TABLE "public"."users" (
   "id" int4 NOT NULL DEFAULT nextval('users_id_seq'::regclass),
   "username" varchar(256) COLLATE "pg_catalog"."default",
   "password" varchar(256) COLLATE "pg_catalog"."default",
-  "update_time" varchar(32) COLLATE "pg_catalog"."default" DEFAULT now()
+  "update_time" varchar(32) COLLATE "pg_catalog"."default" DEFAULT (now())::timestamp(0) without time zone
 )
 ;
 
@@ -34,8 +34,8 @@ CREATE TABLE "public"."users" (
 INSERT INTO "public"."users" VALUES (2, 'robin', '4123560', '2020-06-15 23:17:30');
 INSERT INTO "public"."users" VALUES (5, 'john', '567', '2020-06-16 09:34:42');
 INSERT INTO "public"."users" VALUES (7, 'marry', '123', '2020-06-16 10:19:17');
-INSERT INTO "public"."users" VALUES (11, '李四', '芝麻开门', '2020-06-16 10:29:08');
-INSERT INTO "public"."users" VALUES (1, '张三', '123', '2020-06-29 21:30:40');
+INSERT INTO "public"."users" VALUES (1, '张三', '123', '2020-06-30 08:44:17');
+INSERT INTO "public"."users" VALUES (8, '大张伟', '3252', '2020-06-30 09:07:10');
 
 -- ----------------------------
 -- Primary Key structure for table users
